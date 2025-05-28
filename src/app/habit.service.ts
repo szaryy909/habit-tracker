05d9d932737b688dbbe1945152310ebe82e5174a
uczen <kacperszary06@gmail.com>
2025-05-28 13:14:56 +0200

    logika serwisu

--- src/app/habit.service.ts
+++ src/app/habit.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Habit } from '../app/habit.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HabitService {
+  private readonly STORAGE_KEY = 'habits';
 
-  constructor() { }
+  getHabits(): Habit[] {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
+  }
+
+  addHabit(habit: Habit) {
+    const habits = this.getHabits();
+    habits.push(habit);
+    this.saveHabits(habits);
+  }
+
+  removeHabit(index: number) {
+    const habits = this.getHabits();
+    habits.splice(index, 1);
+    this.saveHabits(habits);
+  }
+
+  private saveHabits(habits: Habit[]) {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(habits));
+  }
 }
