8a2672c77c6e8eaa2e6a3849b7f0c12adb79d1ea
uczen <kacperszary06@gmail.com>
2025-05-28 13:10:24 +0200

    dodanie componentow

---
+++ src/app/habit-item/habit-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './habit-item.component.html',
+  styleUrl: './habit-item.component.css'
+})
+export class HabitItemComponent {
+
+}
