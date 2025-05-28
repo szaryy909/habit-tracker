28292c3b099bdd801a0ed1d56b8b95d7bfd6ce33
uczen <kacperszary06@gmail.com>
2025-05-28 13:24:07 +0200

    polaczenie wszystkiego w appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HabitService } from './habit.service';
+import { Habit } from './habit.model';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>System Zarządzania Nawykami</h1>
+    <app-habit-form (habitAdded)="addHabit($event)"></app-habit-form>
+    <app-habit-list [habits]="habits" (habitRemoved)="removeHabit($event)"></app-habit-list>
+  `
 })
-export class AppComponent {
-  title = 'habit-tracker';
+export class AppComponent implements OnInit {
+  habits: Habit[] = [];
+
+  constructor(private habitService: HabitService) {}
+
+  ngOnInit() {
+    this.habits = this.habitService.getHabits();
+  }
+
+  addHabit(habit: Habit) {
+    this.habitService.addHabit(habit);
+    this.habits = this.habitService.getHabits();
+  }
+
+  removeHabit(index: number) {
+    this.habitService.removeHabit(index);
+    this.habits = this.habitService.getHabits();
+  }
 }
+
